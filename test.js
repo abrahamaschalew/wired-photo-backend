6353e83f023d78dcd3f25a39cba65a6148415609
Abraham Aschalew <abrshaschalew@gmail.com>
2022-04-06 13:22:00 +0300

    Image compression

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
-function example(parameter) {
-  console.log(parameter); // Output = foo
+// program to split array into smaller chunks
+
+function splitIntoChunk(arr, chunk) {
+  let chunks = [];
+  while (arr.length > 0) {
+    let tempArray;
+    tempArray = arr.splice(0, chunk);
+    chunks.push(tempArray);
+  }
+
+  return chunks;
 }
-
-const argument = "foo";
-
-example(argument);
